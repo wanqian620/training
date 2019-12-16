774ad5851b52047d313d3c06420a8bde7f26d0f0
weijun <weijunfancy@163.com>
2019-12-16 17:58:00 +0800

    feat: demo

---
+++ demo/src/main/java/com/example/utils/demo.java
@@ -0,0 +1,9 @@
+package com.example.utils;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("11111");
+
+        System.out.println("2222");
+    }
+}
