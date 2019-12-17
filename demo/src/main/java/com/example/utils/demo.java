b88eb1c500670430f1b648cee8b487d81b244b32
weijun <weijunfancy@163.com>
2019-12-17 09:31:04 +0800

    fix: 4444

--- demo/src/main/java/com/example/utils/demo.java
+++ demo/src/main/java/com/example/utils/demo.java
@@ -2,10 +2,12 @@ package com.example.utils;
 
 public class demo {
     public static void main(String[] args) {
-        System.out.println("11111");
+        System.out.println("feat: 11111");
 
-        System.out.println("2222");
+        System.out.println("feat: 2222");
 
-        System.out.println("33333");
+        System.out.println("feat: 33333");
+
+        System.out.println("fix: 44444");
     }
 }
